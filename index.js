dc4b22c6beeeda19e5c42b4884d53c511c42ecd6
Zhino Salam <zhinosalam188@gmail.com>
2021-05-19 22:05:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ class Formatter {
         console.log('My method is static!');
     }
 }
+static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    const checkArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    let returnString = string.split(" ").map((word, index) => {
+      if (index === 0) {
+        return this.capitalize(word);
+      } else {
+        if (!checkArray.includes(word)) {
+          return this.capitalize(word);
+        } else {
+          return word;
+        }
+      }
+    })
+    return returnString.join(" ");
+  }
+} 
